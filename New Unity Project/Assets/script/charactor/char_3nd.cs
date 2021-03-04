9dd54c6531ee6f0c82be68fe8aaa6050b610d387
Sorrel <43405887+Hing-Gu@users.noreply.github.com>
2021-03-05 03:07:46 +0900

    다시처음

---
+++ New Unity Project/Assets/script/charactor/char_3nd.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class char_3nd : MonoBehaviour
+{
+    public float speed;
+    public Vector2 speed_vec;
+
+
+    void Start()
+    {
+        WhatFloor.floor = 3;
+    }
+    void Update()
+    {
+        speed_vec = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            speed_vec.x -= speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            speed_vec.x += speed;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            speed_vec.y += speed;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            speed_vec.y -= speed;
+
+        }
+        GetComponent<Rigidbody2D>().velocity = speed_vec;
+    }
+
+}
+
