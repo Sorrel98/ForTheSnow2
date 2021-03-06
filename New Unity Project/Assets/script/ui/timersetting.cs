61f34cd880d3fb3c7b498f0e98d384ea083a3260
Sorrel <43405887+Hing-Gu@users.noreply.github.com>
2021-03-07 02:23:13 +0900

    timer

---
+++ New Unity Project/Assets/script/ui/timersetting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timersetting : MonoBehaviour
+{
+    private float GameTime = 11;
+    public Text GameTimeText =null;
+
+    void Update()
+    {
+        if ((int)GameTime == 0)
+        {
+            GameTimeText.text = "게임종료";
+            Debug.Log("게임 종료");
+        }else
+        {
+            GameTime -= Time.deltaTime;
+            Debug.Log((int)GameTime);
+            GameTimeText.text = "Time : " + (int)GameTime;
+        }
+    }
+}
