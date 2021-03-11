3c1a413959edcd767af49980f626b443b6b7ff43
fish2203 <fish2203@gmail.com>
2021-03-11 21:12:37 +0900

    전역 인벤토리 전환

---
+++ New Unity Project/Assets/script/LoadingTimer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingTimer : MonoBehaviour
+{
+    double timer;
+    int waitingTime;
+ 
+    void Start()
+    {
+        timer = 0.0;
+        waitingTime = 2;
+    }
+ 
+    void Update()
+    {
+        timer += Time.deltaTime;
+   
+        if(timer > waitingTime)
+    {
+        SceneManager.LoadScene("1층_중앙");
+    }
+}
+}
