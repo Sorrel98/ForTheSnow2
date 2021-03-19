7c99044fa4853e0cd8fe5ce88e66f314650a9ab4
Sorrel <43405887+Hing-Gu@users.noreply.github.com>
2021-03-20 04:54:31 +0900

    raycast오류해결

---
+++ New Unity Project/Assets/script/class/history/history.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class history : MonoBehaviour
+{
+    public GameObject Target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(WhatClass.classroom == 102)
+        {
+            Target.SetActive(true);
+        }
+        else
+        {
+            Target.SetActive(false);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("클릭 감지");
+            SceneManager.LoadScene("역사과목");
+        }
+    }
+}
